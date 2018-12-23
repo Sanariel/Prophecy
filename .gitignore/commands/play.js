bd6801942064455ce07c8200bc12924d30678b24
Sanariel <46084511+Sanariel@users.noreply.github.com>
2018-12-23 18:08:55 +0100

    Create play.js

---
+++ .gitignore/commands/play.js
@@ -0,0 +1,27 @@
+const Command = require('./command')
+const youtubeStream = require('youtube-audio-stream')
+
+module.exports = class Play extends Command {
+
+    static match(message) {
+        return message.content.startsWith('!play')
+    }
+    static action(message) {
+        let voiceChannel = message.guild.channels
+            .filter(function (channel) { return channel.type === 'voice' })
+            .first()
+        let args = message.content.split(' ')
+        voiceChannel
+            .join()
+            .then(function (connection) {
+                let stream = YoutubeStream(args[1])
+                connection.playStream(stream).on('end', function () {
+                    connection.disconnect()
+                })
+
+            })
+
+
+    }
+
+}
