177f68f4862d085037617a55e9bdce951ad6e351
Sanariel <46084511+Sanariel@users.noreply.github.com>
2018-12-24 11:39:13 +0100

    Update gouvernement.js

--- .gitignore/commands/gouvernement.js
+++ .gitignore/commands/gouvernement.js
@@ -3,7 +3,7 @@ const Command = require('./command.js')
 module.exports = class Gouverenement extends Command {
 
     static match(message) {
-        return message.content('gouvernement')
+        return message.content.startsWith('!gouvernement')
     }
     static action(message) {
         message.reply('Les Bouffeurs de Caviar')
