d60ad010db4f3b59b119b71b0bf2754c58e1cab5
Sanariel <46084511+Sanariel@users.noreply.github.com>
2018-12-24 11:39:45 +0100

    Update rebel.js

--- .gitignore/commands/rebel.js
+++ .gitignore/commands/rebel.js
@@ -3,7 +3,7 @@ const Command = require('./command.js')
 module.exports = class Rebel extends Command {
 
     static match(message) {
-        return message.content('rebel')
+        return message.content.startsWith('!rebel')
     }
     static action(message) {
         message.reply('Les Consommateurs LowCost Top Budget')
