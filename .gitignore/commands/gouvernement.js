402f632c419543e0724b7c556d0adc5ddf515e97
Sanariel <46084511+Sanariel@users.noreply.github.com>
2018-12-24 10:57:56 +0100

    Create gouvernement.js

---
+++ .gitignore/commands/gouvernement.js
@@ -0,0 +1,12 @@
+const Command = require('./command.js')
+
+module.exports = class Gouverenement extends Command {
+
+    static match(message) {
+        return message.content.startsWith('!gouvernement')
+    }
+    static action(message) {
+        message.reply('Les Bouffeurs de Caviar')
+    }
+
+}
